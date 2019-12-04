e1596f15d591dae0c154f3fd1e2421c02714f666
Vladimir Migashko <migashko@gmail.com>
2019-12-04 14:57:24 +0300

    up 1

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const payload2 = JSON.stringify(github, undefined, 2)
+  console.log(`The event payload: ${payload2}`);
+  console.log(`GITHUB_TOKEN: ${process.env.GITHUB_TOKEN}`);
 } 
 catch (error) 
 {
