f99c411965cca66efdef29f3d11ecf3d01337834
Vladimir Migashko <migashko@gmail.com>
2019-12-04 15:15:02 +0300

    up 4

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ try {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
   // See https://developer.github.com/v3/issues/#create-an-issue
-  const { data } = await octokit.request("POST /repos/:owner/:repo/issues", {
+  //const { data } = await octokit.request("POST /repos/:owner/:repo/issues", {
+  const { data } = octokit.request("POST /repos/:owner/:repo/issues", {
     owner,
     repo,
     title: "My test issue"
